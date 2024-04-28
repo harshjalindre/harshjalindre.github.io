ed7213199d58c09a09e807a04ec5665d1447ac05
harshjalindre <92078558+harshjalindre@users.noreply.github.com>
2024-04-29 03:03:22 +0530

    first commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Kudos Friends, I am <span className="purple">Harsh Jalindre </span>
+            from <span className="purple"> Pune, Maharashra, India.</span>
+            <br />
+            I am currently employed as a Software Developer at TechLeaper Systems Pvt. Ltd.
+            <br />
+            I have completed my eductaion in M.Sc.(Computer Science) from Pune University.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Crafting
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "#f10132" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Harsh</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Particle.js
@@ -0,0 +1,107 @@
+import React from "react";
+import Particles from "react-tsparticles";
+
+function Particle() {
+  return (
+    // <Particles
+    //   id="tsparticles"
+    //   params={{
+    //     particles: {
+    //       number: {
+    //         value: 160,
+    //         density: {
+    //           enable: true,
+    //           value_area: 1500,
+    //         },
+    //       },
+    //       line_linked: {
+    //         enable: false,
+    //         opacity: 0.03,
+    //       },
+    //       move: {
+    //         direction: "right",
+    //         speed: 0.05,
+    //       },
+    //       size: {
+    //         value: 1,
+    //       },
+    //       opacity: {
+    //         anim: {
+    //           enable: true,
+    //           speed: 1,
+    //           opacity_min: 0.05,
+    //         },
+    //       },
+    //     },
+    //     interactivity: {
+    //       events: {
+    //         onclick: {
+    //           enable: true,
+    //           mode: "push",
+    //         },
+    //       },
+    //       modes: {
+    //         push: {
+    //           particles_nb: 1,
+    //         },
+    //       },
+    //     },
+    //     retina_detect: true,
+    //   }}
+    // />
+    <Particles
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 160,
+	            "density": {
+	                "enable": false
+	            }
+	        },
+	        "size": {
+	            "value": 3,
+	            "random": true,
+	            "anim": {
+	                "speed": 4,
+	                "size_min": 0.3
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 1,
+	            "direction": "top",
+	            "out_mode": "out"
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "bubble"
+	            },
+	            "onclick": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        },
+	        "modes": {
+	            "bubble": {
+	                "distance": 250,
+	                "duration": 2,
+	                "size": 0,
+	                "opacity": 0
+	            },
+	            "repulse": {
+	                "distance": 400,
+	                "duration": 4
+	            }
+	        }
+	    }
+	}} />
+  );
+}
+
+export default Particle;
